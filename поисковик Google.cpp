45e27889417f0bdf120ce37594cef1d5f4753011
arinasenozhenskaya <losadgnedaa@gmail.com>
2026-07-14 16:06:43 +0300

    поисковик

--- поисковик Google.cpp
+++ поисковик Google.cpp
@@ -6,7 +6,7 @@ int main() {
     HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
     HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    // Ваши строки соответствия
+    
     std::wstring en = L"qwertyuiop[]asdfghjkl;'zxcvbnm,./";
     std::wstring ru = L"йцукенгшщзхъфывапролджэячсмитьбю.";
 
@@ -22,15 +22,15 @@ int main() {
         size_t p_ru = ru.find(c);
 
         if (p_en != std::wstring::npos) {
-            // Если символ найден в английской строке, меняем на русский
+         
             result += ru[p_en];
         }
         else if (p_ru != std::wstring::npos) {
-            // Если найден в русской, меняем на английский
+          
             result += en[p_ru];
         }
         else {
-            // Если символ не найден (пробел, цифра), оставляем как есть
+          
             result += c;
         }
     }
